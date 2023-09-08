966269fb8b6d83d1e921cf960e4e69176f6cc3a8
gg_ <89791105+ggvhs@users.noreply.github.com>
2023-09-08 18:17:45 -0400

    Moved logic from within songRouts.js to songController.js for better readability for post put and delete routes

--- backend/controllers/songController.js
+++ backend/controllers/songController.js
@@ -1,7 +1,37 @@
+//@description  Get songs
+//@route        Get /api/songs
+//@access       Private
 const getSongs = (req,res) => {
     res.status(200).json({message:'Get Songs'})
 }
 
+
+//@description  Set songs
+//@route        POST /api/songs
+//@access       Private
+const setSong = (req,res) => {
+    res.status(200).json({message:'Set Songs'})
+}
+
+
+//@description  Update songs
+//@route        PUT /api/songs
+//@access       Private
+const updateSong = (req,res) => {
+    res.status(200).json({message:`update song ${req.params.id}`})
+}
+
+
+//@description  Delete songs
+//@route        DELETE /api/songs/:id
+//@access       Private
+const deleteSong = (req,res) => {
+    res.status(200).json({message: `delete song ${req.params.id}`})
+}
+
 module.exports = {
-    getSongs
+    getSongs,
+    setSong,
+    updateSong,
+    deleteSong
 }
--- backend/routes/songRoutes.js
+++ backend/routes/songRoutes.js
@@ -1,17 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const {getSongs} = require('../controllers/songController')
+const {getSongs , setSong , updateSong, deleteSong} = require('../controllers/songController')
 
 router.get('/', getSongs)
 
-router.post('/', (req,res) =>{
-    res.status(200).json({message:'Set Songs'})
-})
-router.put('/:id', (req,res) =>{
-    res.status(200).json({message:`update goal ${req.params.id}`})
-})
-router.delete('/:id', (req,res) =>{
-    res.status(200).json({message:`delete goal ${req.params.id}`})
-})
+router.post('/', setSong)
+
+router.put('/:id', updateSong)
+
+router.delete('/:id', deleteSong)
+
 
 module.exports = router
