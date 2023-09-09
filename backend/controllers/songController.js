a347151f8664af65c1bb96f7e3c0a5db1a0f398f
gg_ <89791105+ggvhs@users.noreply.github.com>
2023-09-09 11:31:18 -0400

    goal controller now uses asyncHandler

--- backend/controllers/songController.js
+++ backend/controllers/songController.js
@@ -1,37 +1,40 @@
+const asyncHandler = require('express-async-handler')
+
+
 //@description  Get songs
 //@route        Get /api/songs
 //@access       Private
-const getSongs = (req,res) => {
+const getSongs = asyncHandler( async (req,res) => {
     res.status(200).json({message:'Get Songs'})
-}
+})
 
 
 //@description  Set songs
 //@route        POST /api/songs
 //@access       Private
-const setSong = (req,res) => {
+const setSong = asyncHandler(async (req,res) => {
     if(!req.body.text){
         res.status(400)
         throw new Error('Please add a text field ')
     }
     res.status(200).json({message:'Set Songs'})
-}
+})
 
 
 //@description  Update songs
 //@route        PUT /api/songs
 //@access       Private
-const updateSong = (req,res) => {
+const updateSong = asyncHandler(async (req,res) => {
     res.status(200).json({message:`update song ${req.params.id}`})
-}
+})
 
 
 //@description  Delete songs
 //@route        DELETE /api/songs/:id
 //@access       Private
-const deleteSong = (req,res) => {
+const deleteSong = asyncHandler(async (req,res) => {
     res.status(200).json({message: `delete song ${req.params.id}`})
-}
+})
 
 module.exports = {
     getSongs,
