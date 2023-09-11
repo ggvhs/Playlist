c0271209eda330525daca315ffe10fa6266110e0
gg_ <89791105+ggvhs@users.noreply.github.com>
2023-09-11 09:54:04 -0400

    routes are completed

--- backend/controllers/songController.js
+++ backend/controllers/songController.js
@@ -16,11 +16,16 @@ const getSongs = asyncHandler( async (req,res) => {
 //@route        POST /api/songs
 //@access       Private
 const setSong = asyncHandler(async (req,res) => {
-    if(!req.body.text){
+    if(!req.body.songName || !req.body.artistName){
         res.status(400)
         throw new Error('Please add a text field ')
     }
-    res.status(200).json({message:'Set Songs'})
+
+    const song = await Song.create({
+        songName: req.body.songName,
+        artistName: req.body.artistName
+    }) 
+    res.status(200).json(song)
 })
 
 
@@ -28,7 +33,16 @@ const setSong = asyncHandler(async (req,res) => {
 //@route        PUT /api/songs
 //@access       Private
 const updateSong = asyncHandler(async (req,res) => {
-    res.status(200).json({message:`update song ${req.params.id}`})
+    const song = await Song.findById(req.params.id)
+
+    if(!song) {
+        res.status(400)
+        throw new Error('Song not found')
+    }
+
+    const updataedGoal = await Song.findByIdAndUpdate(req.params.id,req.body,{new: true,})
+
+    res.status(200).json(updataedGoal)
 })
 
 
@@ -36,7 +50,16 @@ const updateSong = asyncHandler(async (req,res) => {
 //@route        DELETE /api/songs/:id
 //@access       Private
 const deleteSong = asyncHandler(async (req,res) => {
-    res.status(200).json({message: `delete song ${req.params.id}`})
+    const song = await Song.findByIdAndRemove(req.params.id)
+
+    if(!song) {
+        res.status(400)
+        throw new Error('Song not found')
+    }
+
+    
+
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
