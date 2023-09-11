ecae457b58e4a479820ea7a437ad45d7570d8361
gg_ <89791105+ggvhs@users.noreply.github.com>
2023-09-11 13:39:45 -0400

    I am now able to register users through my backend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,16 +4,16 @@ const mongoose = require('mongoose')
 const userSchema = mongoose.Schema({
     name: {
         type: String,
-        require: [true, 'Please add a name']
+        required: [true, 'Please add a name']
     },
     email: {
         type: String,
-        require: [true, 'Please add a email'],
+        required: [true, 'Please add a email'],
         unique: true
     },
     password: {
         type: String,
-        require: [true, 'Please add a password']
+        required: [true, 'Please add a password']
     },
 },
 {
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const { registerUser, loginUser, getMe } = require('../controllers/userController')
 
 router.post('/',  registerUser)
-router.post('/login',  loginUser)
-router.post('/me', getMe)
+router.post('/login', loginUser)
+router.get('/me', getMe)
 
 module.exports = router
