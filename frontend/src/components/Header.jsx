505de861257e12c507a29db1e1b2cb6cd50a639e
gg_ <89791105+ggvhs@users.noreply.github.com>
2023-09-13 16:54:14 -0400

    front end components altered

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,23 +1,46 @@
-import {Link} from 'react-router-dom'
+
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout, reset } from '../features/auth/authSlice'
 
 function Header() {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { user } = useSelector((state) => state.auth)
+
+  const onLogout = () => {
+    dispatch(logout())
+    dispatch(reset())
+    navigate('/')
+  }
+
   return (
-    <header>
-        <div>
-            <Link to='/'>Playlister</Link>
-        </div>
-        <ul>
+    <header className='header'>
+      <div className='logo'>
+        <Link to='/'>Playlister</Link>
+      </div>
+      <ul>
+        {user ? (
+          <li>
+            <button className='btn' onClick={onLogout}>
+               Logout
+            </button>
+          </li>
+        ) : (
+          <>
             <li>
-                <Link to='/login'>
-                    Login
-                </Link>
+              <Link to='/login'>
+                 Login
+              </Link>
             </li>
             <li>
-                <Link to='/register'>
-                    Register
-                </Link>
+              <Link to='/register'>
+                 Register
+              </Link>
             </li>
-        </ul>
+          </>
+        )}
+      </ul>
     </header>
   )
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,61 +1,112 @@
-import { useState, useEffect } from "react";
+import {useState, useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { login, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
-import React from "react";
+
+
 
 function Login() {
   const [formData, setFormData] = useState({
-    email: "",
-    password: ""
-  });
+ 
+    email: '',
+    password: '',
 
-  const {  email, password } = formData;
+  })
+
+  const{email,password} = formData
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
 
   const onChange = (e) => {
     setFormData((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-
+        ...prevState,
+        [e.target.name]: e.target.value,
     }))
   }
 
   const onSubmit = (e) =>{
     e.preventDefault()
+
+    const userData = {
+      email,
+      password
+    }
+
+    dispatch(login(userData))
   }
+
+  if(isLoading){
+    return <Spinner />
+  }
+
 
   return (
     <>
-      <section>
-        <h1>Login</h1>
-        <p>Login and build you playlist</p>
+      <section className='heading'>
+        <h1>
+           Login
+        </h1>
+        <p>Login in and start setting goals</p>
       </section>
 
-      <section>
+      <section className='form'>
         <form onSubmit={onSubmit}>
-          <div>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-            placeholder="Enter your email"
-            onChange={onChange}
-          />
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            placeholder="Enter your password"
-            onChange={onChange}
-          />
+        
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              value={email}
+              placeholder='Enter your email'
+              onChange={onChange}
+            />
           </div>
-          <div>
-            <button type="submit">Submit</button>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              placeholder='Enter password'
+              onChange={onChange}
+            />
+          </div>
+          
+          
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>
+              Submit
+            </button>
           </div>
         </form>
       </section>
     </>
-  );
+  )
+  
 }
 
-export default Login;
+
+export default Login
